f9a8a29d362a396b17eb04d2223fab3fa7a2519f
santifont <sanalffer@alu.edu.gva.es>
2026-02-03 11:40:19 +0100

    Making the scene

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void TitleScreen()
+    {
+        SceneManager.LoadScene("TitleScreen");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
